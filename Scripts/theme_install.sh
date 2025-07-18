7983470e7b64bab438a65dcc1ea1fc242dabb039
jinykim47 <129277561+jinykim47@users.noreply.github.com>
2025-07-18 22:33:05 +1000

    my changes

---
+++ Scripts/theme_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Theme installs
+hydectl theme import --name "Abyssal-Wave" --url "https://github.com/Itz-Abhishek-Tiwari/Abyssal-Wave"
+hydectl theme import --name "Ever-Blushing" --url "https://github.com/rishav12s/Ever-Blushing"
+hydectl theme import --name "One-Dark" --url "https://github.com/RAprogramm/HyDe-Themes/tree/One-Dark"
+hydectl theme import --name "Pixel-Dream" --url "https://github.com/rishav12s/Pixel-Dream"
+hydectl theme import --name "Monokai" --url "https://github.com/mahaveergurjar/Theme-Gallery/tree/Monokai"
+hydectl theme import --name "Eternal-Artic" --url "https://github.com/rishav12s/Eternal-Arctic"
+hydectl theme import --name "BlueSky" --url "https://github.com/richen604/BlueSky"
+hydectl theme import --name "Code-Garden" --url "https://github.com/jacobfranco/Code-Garden"
+hydectl theme import --name "Crimson-Blue" --url "https://github.com/amit-0i/Crimson-Blue"
+hydectl theme import --name "Decay-Green" --url "https://github.com/HyDE-Project/hyde-themes/tree/Decay-Green"
+hydectl theme import --name "Nightbrew" --url "https://github.com/jackpawlik1/Nightbrew"
+hydectl theme import --name "Piece-Of-Mind" --url "https://github.com/Maroc02/Piece-Of-Mind"
+hydectl theme import --name "Vanta-Black" --url "https://github.com/rishav12s/Vanta-Black"
